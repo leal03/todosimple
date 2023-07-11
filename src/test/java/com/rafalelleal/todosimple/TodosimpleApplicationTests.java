1cae6f52effaece83c42ed358633ac29d34debdc
Rafael Gonçalves Leal <leal.rafael1204@gmail.com>
2023-07-11 17:08:24 -0300

    Primeiro commit, adicionando os arquivos

---
+++ src/test/java/com/rafalelleal/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafalelleal.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
