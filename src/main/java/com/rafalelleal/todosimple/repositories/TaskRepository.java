fa2c0f343e754a71dd4343fd6cfe83112c8d077d
Rafael Gonçalves Leal <leal.rafael1204@gmail.com>
2023-08-15 13:42:57 -0300

    Adicionando os Repositories de User e Task

---
+++ src/main/java/com/rafalelleal/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.rafalelleal.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafalelleal.todosimple.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+    List<Task> findByUser_Id(Long id);
+    
+}
