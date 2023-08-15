ee2b524bbf04b81de07881c5b6d9fbfcd134fd8d
Rafael Gonçalves Leal <leal.rafael1204@gmail.com>
2023-08-14 21:53:07 -0300

    Criando Task Model

---
+++ src/main/java/com/rafalelleal/todosimple/models/Task.java
@@ -0,0 +1,87 @@
+package com.rafalelleal.todosimple.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.lang.NonNull;
+import java.util.Objects;
+
+@Entity
+@Table(name = "task")
+public class Task {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @Column(name = "description", length = 255, nullable = false)
+    @NonNull
+    @NotEmpty
+    @Size(min = 1, max = 255)
+    private String description;
+
+
+    public Task() {
+    }
+
+    public Task(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Task id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == null){
+            return false;
+        }
+        if(obj == this){
+            return true;
+        }
+        if(!(obj instanceof Task)){
+            return false;
+        }
+        Task other = (Task) obj;
+        if(this.id == null){
+            if(other.id != null){
+                return false;
+            }else if(!this.id.equals(other.id)){
+                return false;
+            }
+        }
+        return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user) && Objects.equals(this.description, other.description);
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (this.id == null ? 0 : this.id.hashCode());
+        return result;
+    }
+    
+}
